cb4f51e9e4377fbc1f9f723a9d0fc10e7642bd71
kalyakinaa <nastyakalyakinann@gmail.com>
2025-03-08 02:40:12 +0300

    Formatting changes (circle.h)

--- include/circle.h
+++ include/circle.h
@@ -7,25 +7,25 @@
 constexpr double pi = 3.1415926535;
 
 class Circle {
- double radius;
- double ference;
- double area;
+  double radius;
+  double ference;
+  double area;
 
-public:
- Circle() :radius(0.0), ference(0.0), area(0.0) {};
- explicit Circle(double r);
- void setRadius(double r);
- void setFerence(double f);
- void setArea(double a);
- double getRadius();
- double getFerence();
- double getArea();
+ public:
+  Circle() : radius(0.0), ference(0.0), area(0.0) {};
+  explicit Circle(double r);
+  void setRadius(double r);
+  void setFerence(double f);
+  void setArea(double a);
+  double getRadius();
+  double getFerence();
+  double getArea();
 
-private:
- double calculateRadiusFromFerence(double f);
- double calculateRadiusFromArea(double a);
- double calculateFerence(double r);
- double calculateArea(double r);
+ private:
+  double calculateRadiusFromFerence(double f);
+  double calculateRadiusFromArea(double a);
+  double calculateFerence(double r);
+  double calculateArea(double r);
 };
 
 #endif  // INCLUDE_CIRCLE_H_
