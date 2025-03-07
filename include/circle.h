01aa55d6cd301897cbb9633e75b6ba0750a9f726
kalyakinaa <nastyakalyakinann@gmail.com>
2025-03-08 02:27:14 +0300

    Creating class Circle

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,32 @@
 // Copyright 2022 UNN-CS
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
+#include <cmath>
 #include <cstdint>
 
+constexpr double pi = 3.1415926535;
+
+class Circle {
+	double radius;
+	double ference;
+	double area;
+
+public:
+	Circle() :radius(0.0), ference(0.0), area(0.0) {};
+	explicit Circle(double r);
+	void setRadius(double r);
+	void setFerence(double f);
+	void setArea(double a);
+	double getRadius();
+	double getFerence();
+	double getArea();
+
+private:
+	double calculateRadiusFromFerence(double f);
+	double calculateRadiusFromArea(double a);
+	double calculateFerence(double r);
+	double calculateArea(double r);
+
+};
 
 #endif  // INCLUDE_CIRCLE_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,60 @@
 // Copyright 2022 UNN-CS
+#include <cmath>
 #include <cstdint>
 #include "circle.h"
 
+Circle::Circle(double r) {
+	setRadius(r);
+}
+
+void Circle::setRadius(double r) {
+	if (r < 0.0)
+		throw "Invalid radius";
+	radius = r;
+	ference = calculateFerence(r);
+	area = calculateArea(r);
+}
+
+void Circle::setFerence(double f) {
+	if (f < 0.0)
+		throw "Invalid ference";
+	radius = calculateRadiusFromFerence(f);
+	ference = f;
+	area = calculateArea(radius);
+}
+
+void Circle::setArea(double a) {
+	if (a < 0.0)
+		throw "Invalid area";
+	radius = calculateRadiusFromArea(a);
+	ference = calculateFerence(radius);
+	area = a;
+}
+
+double Circle::getRadius() {
+	return radius;
+}
+
+double Circle::getFerence() {
+	return ference;
+}
+
+double Circle::getArea() {
+	return area;
+}
+
+double Circle::calculateRadiusFromFerence(double f) {
+	return f / (2 * pi);
+}
+
+double Circle::calculateRadiusFromArea(double a) {
+	return sqrt(a / pi);
+}
+
+double Circle::calculateFerence(double r) {
+	return 2 * pi * r;
+}
+
+double Circle::calculateArea(double r) {
+	return pi * r * r;
+}
