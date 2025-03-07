8909e0795fb48e19f643395a7364ae6fb4d77c2b
kalyakinaa <nastyakalyakinann@gmail.com>
2025-03-08 02:34:25 +0300

    Formatting changes (circle.h, circle.cpp)

--- src/circle.cpp
+++ src/circle.cpp
@@ -4,57 +4,57 @@
 #include "circle.h"
 
 Circle::Circle(double r) {
-	setRadius(r);
+  setRadius(r);
 }
 
 void Circle::setRadius(double r) {
-	if (r < 0.0)
-		throw "Invalid radius";
-	radius = r;
-	ference = calculateFerence(r);
-	area = calculateArea(r);
+  if (r < 0.0)
+    throw "Invalid radius";
+  radius = r;
+  ference = calculateFerence(r);
+  area = calculateArea(r);
 }
 
 void Circle::setFerence(double f) {
-	if (f < 0.0)
-		throw "Invalid ference";
-	radius = calculateRadiusFromFerence(f);
-	ference = f;
-	area = calculateArea(radius);
+  if (f < 0.0)
+    throw "Invalid ference";
+  radius = calculateRadiusFromFerence(f);
+  ference = f;
+  area = calculateArea(radius);
 }
 
 void Circle::setArea(double a) {
-	if (a < 0.0)
-		throw "Invalid area";
-	radius = calculateRadiusFromArea(a);
-	ference = calculateFerence(radius);
-	area = a;
+  if (a < 0.0)
+    throw "Invalid area";
+  radius = calculateRadiusFromArea(a);
+  ference = calculateFerence(radius);
+  area = a;
 }
 
 double Circle::getRadius() {
-	return radius;
+  return radius;
 }
 
 double Circle::getFerence() {
-	return ference;
+  return ference;
 }
 
 double Circle::getArea() {
-	return area;
+  return area;
 }
 
 double Circle::calculateRadiusFromFerence(double f) {
-	return f / (2 * pi);
+  return f / (2 * pi);
 }
 
 double Circle::calculateRadiusFromArea(double a) {
-	return sqrt(a / pi);
+  return sqrt(a / pi);
 }
 
 double Circle::calculateFerence(double r) {
-	return 2 * pi * r;
+  return 2 * pi * r;
 }
 
 double Circle::calculateArea(double r) {
-	return pi * r * r;
+  return pi * r * r;
 }
