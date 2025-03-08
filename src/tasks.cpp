76fea5576eab7f394474acac0071024fc015c370
kalyakinaa <nastyakalyakinann@gmail.com>
2025-03-08 03:03:51 +0300

    Formatting changes (tasks.cpp)

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -12,5 +12,6 @@ double calculateGapSize() {
 double calculatePoolCost() {
   Circle pool(3);
   Circle pool_with_track(4);
-  return 1000 * (pool_with_track.getArea() - pool.getArea()) + 2000 * pool_with_track.getFerence();
+  return 1000 * (pool_with_track.getArea() - pool.getArea()) 
+    + 2000 * pool_with_track.getFerence();
 }
