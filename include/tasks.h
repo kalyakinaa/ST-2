7bb0648829a9915482a59a7add095e0496ac3f24
kalyakinaa <nastyakalyakinann@gmail.com>
2025-03-08 03:01:43 +0300

    Adding tasks solutions

---
+++ include/tasks.h
@@ -0,0 +1,10 @@
+// Copyright 2022 UNN-CS
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+#include <cstdint>
+#include "circle.h"
+
+double calculateGapSize();
+double calculatePoolCost();
+
+#endif  // INCLUDE_TASKS_H_
---
+++ src/tasks.cpp
@@ -0,0 +1,16 @@
+// Copyright 2022 UNN-CS
+#include <cstdint>
+#include "tasks.h"
+
+double calculateGapSize() {
+  double EarthRadius = 6378100.0;
+  Circle roap(EarthRadius);
+  roap.setFerence(roap.getFerence() + 1);
+  return roap.getRadius() - EarthRadius;
+}
+
+double calculatePoolCost() {
+  Circle pool(3);
+  Circle pool_with_track(4);
+  return 1000 * (pool_with_track.getArea() - pool.getArea()) + 2000 * pool_with_track.getFerence();
+}
